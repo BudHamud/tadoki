1d989448a719b4f87eefaae39b2ddb8c0e919bcf
budhamud <nigitart@gmail.com>
2023-01-16 00:04:36 -0300

    css changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import LangContextProvider from "./context/LangContext";
 import "./index.css";
-import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <LangContextProvider>
